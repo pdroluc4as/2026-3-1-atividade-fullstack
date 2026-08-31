feae217584cf63e62371dfa73ef5b9c687f02512
xarobi <132419016+pdroluc4as@users.noreply.github.com>
2026-08-31 13:48:42 +0000

    ajustes

--- api/src/index.ts
+++ api/src/index.ts
@@ -2,5 +2,8 @@ import 'dotenv/config';
 import { drizzle } from 'drizzle-orm/libsql';
 import { createClient } from '@libsql/client';
 
-const client = createClient({ url: process.env.DB_FILE_NAME! });
+const client = createClient({
+  url: process.env.DB_FILE_NAME ?? 'file:local.db',
+});
+
 export const db = drizzle({ client });
